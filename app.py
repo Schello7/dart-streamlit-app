6e86344dc99f82aeee5daf511e84742fdb6e1666
Schello7 <44027048+Schello7@users.noreply.github.com>
2026-06-12 16:12:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
 import streamlit as st
-import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+import numpy as np
 
-df
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
